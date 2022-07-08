5c670bfebfd1886faecd6598ab3f0304718088cb
unknown <a.abdelazizg@gmail.com>
2022-07-08 20:49:26 +0200

    add some section

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import { Container, Row, Col } from 'reactstrap';
 import CommonSection from '../components/UI/common-section/CommonSection';
 import products from '../assets/fake-data/products';
 import ProductCart from '../components/UI/product-card/ProductCard';
+import '../styles/all-foods.css';
+import ReactPaginate from 'react-paginate';
 
 const AllFoods = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [pageNumber, setPageNumber] = useState(0);
+  const searchedProducts = products.filter( (item) => {
+    let filteredProduct = null; 
+    if(item.title.toLowerCase().includes(searchTerm.toLowerCase()) ) {
+      return filteredProduct = item;
+    } else if(searchTerm === '') {
+      return filteredProduct = products;
+    }
+    return filteredProduct;
+  })
+  const productPerPage = 8;
+  const visitedPage = pageNumber * productPerPage;
+  const displayPage = searchedProducts.slice(visitedPage, visitedPage + productPerPage);
+  const pageCount = Math.ceil(searchedProducts.length / productPerPage);
+
+  const changePage = ({selected}) => {
+    setPageNumber(selected)
+  }
+  
   return (
     <>
       <Helmet title='Foods' >
@@ -14,16 +36,17 @@ const AllFoods = () => {
         <section>
           <Container>
             <Row>
+              
               <Col lg='6' md='6' sm='6'>
-                <div className="search_widget">
-                  <input type="text" placeholder="I'm looking for..." />
+                <div className="search_widget w-50">
+                  <input value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}} type="text" placeholder="I'm looking for..." />
                   <span><i className='ri-search-line'></i></span>
                 </div>
               </Col>
-              
-              <Col lg='6' md='6' sm='6'>
-                <div className="sorting_widget">
-                  <select>
+
+              <Col lg='6' md='6' sm='6' className='mb-4'>
+                <div className="sorting_widget text-end">
+                  <select className='w-50'>
                     <option>Default</option>
                     <option value="ascending">A-Z</option>
                     <option value="decending">Z-A</option>
@@ -32,14 +55,24 @@ const AllFoods = () => {
                   </select>
                 </div>
               </Col>
-
-              {
-                products.map((item, index)=> (
-                  <Col lg='4' md='6' sm='6' key={index} className='mt-4' >
+              
+              { displayPage.map((item, index)=> (
+                  <Col lg='3' md='4' sm='6' key={index} className='mt-4' >
                     <ProductCart product={item} />
                   </Col>
-                ))
-              }
+              )) }
+
+              {/*  Paginate  */}
+              <div>
+                <ReactPaginate
+                  pageCount={pageCount}
+                  onPageChange={changePage}
+                  previousLabel='Prev'
+                  nextLabel='Next'
+                  containerClassName='paginationBttns'
+                  breakLabel='...'
+                />
+              </div>
 
             </Row>
           </Container>
@@ -49,4 +82,4 @@ const AllFoods = () => {
   )
 }
 
-export default AllFoods
+export default AllFoods;
--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import { Container, Row, Col } from 'reactstrap';
+import products from '../assets/fake-data/products';
+import { useParams } from 'react-router-dom';
 
 const FoodDetails = () => {
   return (
