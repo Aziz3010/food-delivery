41d0ee0c340350928df58511e02420e5dbf25ae5
unknown <a.abdelazizg@gmail.com>
2022-07-13 04:57:07 +0200

    add Signup & Signin pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import { Container, Row, Col } from 'reactstrap';
+import CommonSection from '../components/UI/common-section/CommonSection';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const loginEmailRef = useRef();
+  const loginPasswordRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Login");
+  }
+
   return (
     <>
       <Helmet title='Login' >
+        <CommonSection title='Login'/>
         <section>
           <Container>
             <Row>
-              <Col>
-                Login
+              <Col lg='12' md='12' sm='12'>
+                <form onSubmit={submitHandler} className='form'>
+                  <div className="form_group">
+                    <input type="email" required placeholder='Email' ref={loginEmailRef} />
+                  </div>
+                  <div className="form_group">
+                    <input type="password" required placeholder='Password' ref={loginPasswordRef} />
+                  </div>
+                  <button type='submit' className='addToCart_btn'>Login</button>
+                  <p className='m-0 mt-3'>Don't have an account? <Link className='text-primary' to='/register'>Signup now</Link></p>
+                </form>
               </Col>
             </Row>
           </Container>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import CommonSection from '../components/UI/common-section/CommonSection';
 
 const Register = () => {
+  const registerFullNameRef = useRef();
+  const registerEmailRef = useRef();
+  const registerPasswordRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Signup");
+  }
+
   return (
     <>
-      <Helmet title='Register' >
+      <Helmet title='Signup' >
+        <CommonSection title='Signup' />
+
         <section>
           <Container>
             <Row>
-              <Col>
-                Register
+              <Col lg='12' md='12' sm='12'>
+                <form onSubmit={submitHandler} className='form'>
+                  <div className="form_group">
+                    <input type="text" required placeholder='Full Name' ref={registerFullNameRef} />
+                  </div>
+                  <div className="form_group">
+                    <input type="email" required placeholder='Email' ref={registerEmailRef} />
+                  </div>
+                  <div className="form_group">
+                    <input type="password" required placeholder='Password' ref={registerPasswordRef} />
+                  </div>
+                  <button type='submit' className='addToCart_btn'>Signup</button>
+                  <p className='m-0 mt-3'>Already have an account? <Link className='text-primary' to='/login'>Login now</Link></p>
+                </form>
               </Col>
             </Row>
           </Container>
